7794870eb1d16c8f6fc34c7b655d673daffbd450
Scorpio-555 <mye18006@byui.edu>
2021-01-30 11:49:38 -0700

    Hello from Jaden

--- HelloFromNate.java
+++ HelloFromNate.java
@@ -2,7 +2,9 @@ package Prove4;
 
 public class HelloFromNate {
     public static void main(){
+
         System.out.println("Hello, my name is Nate");
+        System.out.println("Hello Nate, my name is Jaden");
     }
         
 }
