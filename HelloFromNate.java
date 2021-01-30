36d38eec4ee24e8eb2370423efaf95754950fd67
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2021-01-30 11:54:06 -0700

    Update HelloFromNate.java
    Hello from Jaden

--- HelloFromNate.java
+++ HelloFromNate.java
@@ -3,6 +3,7 @@ package Prove4;
 public class HelloFromNate {
     public static void main(){
         System.out.println("Hello, my name is Nate");
+        System.out.println("Hello Nate, this is Jaden");
     }
         
 }
